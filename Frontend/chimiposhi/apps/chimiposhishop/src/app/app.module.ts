9235a6c34dbfc064c1462621226485865635c55e
Parsa <parsarasooliofficial@gmail.com>
2023-09-04 05:22:40 +0330

    styles / libs

--- Frontend/chimiposhi/apps/chimiposhishop/src/app/app.module.ts
+++ Frontend/chimiposhi/apps/chimiposhishop/src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { AccordionModule } from 'primeng/accordion';
 
 @NgModule({
     declarations: [AppComponent, HomePageComponent, HeaderComponent, FooterComponent],
-    imports: [BrowserModule, RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })],
+    imports: [BrowserModule, RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' }), AccordionModule, BrowserAnimationsModule],
     providers: [],
     bootstrap: [AppComponent],
     exports: [HeaderComponent, FooterComponent]
